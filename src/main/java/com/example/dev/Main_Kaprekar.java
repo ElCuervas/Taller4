ea6b9e0bcac4e5b47fe9e8b02c69d4e4ec47436f
PvllvKuxal <j.contreras26@ufromail.cl>
2024-11-15 11:57:09 -0300

    feat(kaprekarOp): añade primera funcion
    +kaprekarOp pasa las primeras 4 pruebas

--- src/main/java/com/example/dev/Main_Kaprekar.java
+++ src/main/java/com/example/dev/Main_Kaprekar.java
@@ -5,14 +5,44 @@ package com.example.dev;
 // -encoding UTF-8 -charset UTF-8 -docencoding UTF-8
 
 
+import java.io.CharArrayReader;
+import java.util.Arrays;
 
 public class Main_Kaprekar {
 
 public static void main(String[] args){
-    System.out.println("Clase Main");
-
+    int res = kaprekarOp(3524);
+    System.out.println(res);
 }
 
-//aquí van sus funciones:
+public static int kaprekarOp(int number){
+
+    int[] digitos = new int[4];
+
+    //convierte el entero a arreglo
+    int i = digitos.length - 1;
+    while (number > 0) {
+        digitos[i] = number%10;
+        number = number / 10;
+        i--;
+    }
+
+    //ordena de mayor a menor los numeros del arreglo
+    for (int l = 0; l < digitos.length-1; l++) {
+        for (int j = l+1; j < digitos.length ; j++) {
+            if (digitos[j]>digitos[l]){
+                int aux = digitos[j];
+                digitos[j] = digitos[l];
+                digitos[l] = aux;
+            }
+        }
+    }
+
+    //guarda los dos numeros en orden inverso
+    int mayor = digitos[0]*1000 + digitos[1]*100 + digitos[2]*10 + digitos[3];
+    int menor =  digitos[3]*1000 + digitos[2]*100 + digitos[1]*10 + digitos[0];
+
+    return mayor-menor;
+}
 
 }
